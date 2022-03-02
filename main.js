197f8d8119861c3fe9cecb442c3cbdaf3392bc76
guitt/zoom <francisguitt22209@gmail.com>
2022-03-02 16:49:03 -0300

    first comit

---
+++ main.js
@@ -0,0 +1,82 @@
+window.
+addEventListener('load',()=>{
+  setTimeout(()=>{
+    document
+  .getElementById('cover').style.display="none";
+   showApp();
+  },2000) 
+  document
+  .getElementById('cover').style.display="flex";
+     
+}) 
+
+
+
+function showApp(){
+const getInputForm=document.getElementById('Input');
+ 
+ getInputForm
+ .addEventListener('input',
+ (e)=>{
+  const Event = e.target.value;
+  const DispatchEvent =
+    parseFloat(Event);
+  const storageData =
+  window.localStorage
+  .setItem('data',DispatchEvent);
+ });
+ 
+ const Btn = document.getElementById('btn');
+ 
+ Btn
+   .addEventListener('click',
+   ()=>{
+    const getData = window
+     .localStorage
+     .getItem('data');
+     const resultGetData = parseFloat(getData)
+     result(resultGetData);
+     //console.log( resultGetData);
+   })
+
+const UpdateWeb = document.getElementById('updateWeb');
+
+UpdateWeb
+.addEventListener('click',()=>{
+  window.location.reload();
+  clear(0);
+}) 
+
+const Btn2=document.getElementById('btn2')
+.addEventListener('click',()=>{
+  const EraseDate=window.localStorage.removeItem('data');
+  clear(EraseDate);
+  })
+
+
+function result(param){
+  var fact = 1; 
+  while (param > 1) { 
+    fact *= param; 
+    param--; 
+  } 
+ document
+ .getElementById('screen')
+ .innerHTML=fact; 
+}
+const clear=(er)=>{
+ const Er=er;
+ if(Er){
+ document
+ .getElementById('screen')
+ .innerHTML=Er;
+   
+ }
+ else{
+  document
+    .getElementById('screen')
+    .innerHTML = 0;
+ }
+}
+}
+
